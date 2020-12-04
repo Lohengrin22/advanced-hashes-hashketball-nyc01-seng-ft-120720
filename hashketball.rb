ae34614e30e6c735447d80de6dc2df6a8aa2c9e9
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-04 22:20:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,7 +159,7 @@ def num_points_scored(name)
   def team_names
     teams = []
   game_hash.each do |key, stats|
-    stats[:team_name] << teams.to_s
+    stats[:team_name].map do |i|
     binding.pry
     end 
   end
